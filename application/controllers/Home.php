331cd0212b0b6f01b567349f21bf33c254ba01ec
wssedana <ws.sedana94@gmail.com>
2020-07-29 17:46:19 +0800

    menambahkan file Home.php

---
+++ application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class posting extends CI_Controller
+{
+    public function __construct()
+    {
+        date_default_timezone_set('Asia/Makassar');
+        parent::__construct();
+        //nama function helpers yg kita buat, nama ini bisa disesuaikan dgn keinginan.
+    }
+    public function index()
+    {
+?>
+        <h1>Hellow World !!!</h1>
+<?php
+    }
+}
